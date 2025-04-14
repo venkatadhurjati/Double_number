90d6aae73e793edafc538cbd45d982f1ff961628
Venkata Rayudu Dhurjati <venkatarayududhurjati@Venkatas-MacBook-Pro.local>
2025-04-14 09:36:22 -0500

    5900

---
+++ python/double_number.py
@@ -0,0 +1,9 @@
+
+
+def double_number(n):
+    return n * 2
+
+# example usage
+specified_number = 11
+result = double_number(specified_number)
+print (f"The double of {specified_number} is {result}")
